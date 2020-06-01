3b89558b8c110ad6bb85714ab04ced12118a9c8d
Razdabudzka <razdabudzka@icloud.com>
2020-06-01 23:32:15 +0300

    commit

---
+++ Matrix Task/Controllers/DetailsTableVC.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsTableVC.swift
+//  Matrix Task
+//
+//  Created by Veranika Razdabudzka on 6/1/20.
+//  Copyright © 2020 Veranika Razdabudzka. All rights reserved.
+//
+
+import UIKit
+
+class DetailsTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
