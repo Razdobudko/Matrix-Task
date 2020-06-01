317bc77e1ccf5f035a851f592ec0ea189847354d
Razdabudzka <razdabudzka@icloud.com>
2020-06-01 23:04:52 +0300

    Initial Commit

---
+++ Matrix Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Matrix Task
+//
+//  Created by Veranika Razdabudzka on 6/1/20.
+//  Copyright © 2020 Veranika Razdabudzka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
